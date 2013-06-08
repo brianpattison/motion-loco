198c88b8e05dabd866bf1201407762d01b3a466a
Brian Pattison <brian@brianpattison.com>
2013-06-07 22:27:43 -0500

    Add basic tests for serialization

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -22,4 +22,28 @@ describe "Loco::Model" do
     @episodes.is_a?(Loco::RecordArray).should.equal true
   end
   
+  it "should include the root on serialization" do
+    @show = Show.find(1)
+    @hash = @show.serialize
+    @hash[:show].nil?.should.equal false
+  end
+  
+  it "should accept an option to not include root" do
+    @show = Show.find(1)
+    @hash = @show.serialize(root: false)
+    @hash[:show].nil?.should.equal true
+  end
+  
+  it "should accept an option to change the root used" do
+    @show = Show.find(1)
+    @hash = @show.serialize(root: 'podcast')
+    @hash[:podcast].nil?.should.equal false
+  end
+  
+  it "should accept option to include ID on serialization" do
+    @show = Show.find(1)
+    @hash = @show.serialize(include_id: true)
+    @hash[:show][:id].should.equal 1
+  end
+  
 end
