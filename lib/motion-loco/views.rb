91e5a38d509a6bb469a7e5c127ab3bb2f4a9b366
Brian Pattison <brian@brianpattison.com>
2013-09-13 11:24:57 -0500

    Add convienence method for text align in uilabel

--- lib/motion-loco/views.rb
+++ lib/motion-loco/views.rb
@@ -23,6 +23,46 @@ module Loco
     class Label < UILabel
       include Resizable
       include Observable
+      
+      def text_align
+        case self.textAlignment
+        when NSTextAlignmentLeft
+          'left'
+        when NSTextAlignmentCenter
+          'center'
+        when NSTextAlignmentRight
+          'right'
+        when NSTextAlignmentJustified
+          'justified'
+        when NSTextAlignmentNatural
+          'natural'
+        end
+      end
+      alias_method :textAlign, :text_align
+      
+      def text_align=(alignment)
+        case alignment
+        when 'left'
+          self.textAlignment = NSTextAlignmentLeft
+        when 'center'
+          self.textAlignment = NSTextAlignmentCenter
+        when 'right'
+          self.textAlignment = NSTextAlignmentRight
+        when 'justified'
+          self.textAlignment = NSTextAlignmentJustified
+        when 'natural'
+          self.textAlignment = NSTextAlignmentNatural
+        end
+      end
+      alias_method :textAlign=, :text_align=
+      
+      def textAlignment=(alignment)
+        if alignment.is_a? String
+          self.text_align = alignment
+        else
+          super
+        end
+      end
     end
     
     class PageControl < UIPageControl
